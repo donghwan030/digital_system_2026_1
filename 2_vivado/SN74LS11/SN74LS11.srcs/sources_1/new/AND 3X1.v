81c6988658c803ce849882cf8c010e650aa12f78
An Donghwan <donghwan030@gmail.com>
2026-04-10 09:42:08 +0900

    Update date: 26-04-10

---
+++ 2_vivado/SN74LS11/SN74LS11.srcs/sources_1/new/AND 3X1.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/03/2026 02:50:14 PM
+// Design Name: 
+// Module Name: AND 3X1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module AND 3X1(
+
+    );
+endmodule
